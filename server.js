62e66dddb985abdbc2f0201ddc5d3557001be115
ChildOfProfecy <2010030388@klh.edu.in>
2023-02-26 14:14:54 +0530

    Adding mongoDb dataset

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery',true);//to remove connection warning
+dotenv.config()
 const app = express();
-const h = 
+const str = process.env.MONGO_CONNECTION_STRING;
 
-
-
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,function(err){
+    if(err){
+        console.error(err)
+    }else{
+        console.log("DB Connection Success");
+        app.listen(3000,function(){
+            console.log("server running on http://localhost:3000")
+        });
+    }
+});
 
 app.use(express.static("frontend"))//tells to serve all your static files in frontend
 
@@ -13,7 +24,5 @@ app.get("/html",function(req,res){
     res.sendFile(__dirname+"/frontend/index.html");//"__dirname" gives location of server file
 });
 
-app.listen(3000,function(){
-    console.log("server running on http://localhost:3000")
-});
+
 
