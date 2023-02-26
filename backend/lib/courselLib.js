11a1b9e08904c1f7663630e311b0730108da3a76
ChildOfProfecy <2010030388@klh.edu.in>
2023-02-26 15:11:12 +0530

    Create and upadte functions added

--- backend/lib/courselLib.js
+++ backend/lib/courselLib.js
@@ -16,3 +16,12 @@ module.exports.createFirstCourse = async function(){
     await course.save();   
 }
 
+module.exports.createCourse = async function(){
+    const course = await courseModel(courseInput);
+    
+    await course.save();   
+}
+
+module.exports.updateCourse = async function(courseId, updatedInput){
+    await courseModel.findOneAndUpdate({_id:courseId},updateInput);
+}
